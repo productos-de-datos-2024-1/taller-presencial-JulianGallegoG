68d0131fa1b18abcbabcb98e90d9072f6dc70dc1
Julián Gallego <jugallegog@unal.edu.co>
2024-04-16 17:22:46 -0500

    Final try

--- src/4_model/train.py
+++ src/4_model/train.py
@@ -1,28 +1,32 @@
 """Entrenamiento de un modelo de regresión usando sklearn.
+
 ```bash
-$ python src/4_model/train.py --help
+$ python3 src/model/train.py --help
 usage: train.py [-h] [--max_features MAX_FEATURES] [--cv CV] [--max_neighbors MAX_NEIGHBORS]
 
 House Prices Model Training
 
 optional arguments:
--h, --help            show this help message and exit
---max_features MAX_FEATURES 
-                      Max number of features to use in the model
---cv CV               Number of cross-validation folds
---max_neighbors MAX_NEIGHBORS
-                      Max number of neighbors to use in the KNN model
-                      
-$ python src/4_model/train.py --max_features 7 --cv 5 --max_neighbors 10
+  -h, --help            show this help message and exit
+  --max_features MAX_FEATURES
+                        Max number of features to use
+  --cv CV               Number of cross-validation folds
+  --max_neighbors MAX_NEIGHBORS
+                        Max number of neighbors to use
+
+                        
+                        
+                        
+                        
 ```
+
 
 """
 
 import argparse
-import glob
 import json
 import logging
-import os.path
+import os
 import pickle
 
 import pandas as pd
@@ -30,15 +34,14 @@ import pkg_resources
 from sklearn.feature_selection import SelectKBest, f_regression
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
-
-# --------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Logging
-# --------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 CONFIG_FILE = "config.json"
@@ -50,15 +53,15 @@ with pkg_resources.resource_stream(__name__, CONFIG_FILE) as f:
     config = json.load(f)
 
 logging.basicConfig(
-    filename=os.path.join(config["logs_dir"], "train_model.log"),
+    filename=os.path.join(config["logs_dir"], config["train_model_log"]),
     level=logging.INFO,
     format="%(asctime)s:%(levelname)s:%(message)s",
 )
 
 
-# --------------------------------------------------------------------------------
-# Model Definition
-# --------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Model definition
+# -----------------------------------------------------------------------------
 
 
 def make_pipeline(estimator):
@@ -66,7 +69,7 @@ def make_pipeline(estimator):
 
     return Pipeline(
         [
-            ("standardscaler", StandardScaler()),
+            ("standarscaler", StandardScaler()),
             ("selectkbest", SelectKBest(score_func=f_regression)),
             ("estimator", estimator),
         ]
@@ -84,7 +87,7 @@ def make_gridsearch(pipeline, param_grid, cv):
 
 
 def make_linear_regressor(max_features, cv):
-    """Create a linear regressor with a given number of features"""
+    """Create a gridsearch with a given pipeline and parameters"""
 
     model = LinearRegression()
     param_grid = {
@@ -96,11 +99,11 @@ def make_linear_regressor(max_features, cv):
 
 
 def make_knn_regressor(max_features, cv, max_neighbors):
-    """Create a KNN regressor with a given number of features and neighbors"""
+    """Create a gridsearch with a given pipeline and parameters"""
 
     model = KNeighborsRegressor()
     param_grid = {
-        "selectkbest__k": range(1, max_features + 1),
+        "selectkbest__k": range(1, max_features),
         "estimator__n_neighbors": range(1, max_neighbors + 1),
     }
     pipeline = make_pipeline(model)
@@ -108,9 +111,9 @@ def make_knn_regressor(max_features, cv, max_neighbors):
     return gridsearch
 
 
-# --------------------------------------------------------------------------------
-# Model Evaluation
-# --------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Model metrics
+# -----------------------------------------------------------------------------
 
 
 def compute_metrics(y_true, y_pred):
@@ -122,17 +125,22 @@ def compute_metrics(y_true, y_pred):
     return mse, mae, r2
 
 
-def reports_metrics(estimator, mse, mae, r2):
+def report_metrics(estimator, mse, mae, r2):
     """Print the metrics of the model"""
 
-    print(f"Estimator: {estimator}")
-    print(f"Mean squared error: {mse}")
-    print(f"Mean absolute error: {mae}")
-    print(f"R2 score: {r2}")
+    print(estimator, ":", sep="")
+    print(f"  MSE: {mse}")
+    print(f"  MAE: {mae}")
+    print(f"   R2: {r2}")
+
+
+# -----------------------------------------------------------------------------
+# Model management
+# -----------------------------------------------------------------------------
 
 
 def load_model_from_disk():
-    """Load the model from disk"""
+    """Load the model from the disk"""
 
     path = os.path.join(config["models_dir"], config["house_prices_model"])
     if os.path.exists(path):
@@ -144,7 +152,7 @@ def load_model_from_disk():
 
 
 def save_model_to_disk(model):
-    """ "Save the model to the disk"""
+    """Save the model to the disk"""
 
     path = os.path.join(config["models_dir"], config["house_prices_model"])
     with open(path, "wb") as file:
@@ -163,9 +171,9 @@ def compare_saved_model_with_new_model(saved_model, new_model):
     return saved_model
 
 
-# --------------------------------------------------------------------------------
-# Data Management
-# --------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Data management
+# -----------------------------------------------------------------------------
 
 
 def load_train_and_test_datasets():
@@ -195,6 +203,7 @@ def load_train_and_test_datasets():
 
 
 def get_args_from_command_line():
+    """Get the arguments from the command line"""
 
     parser = argparse.ArgumentParser(description="House Prices Model Training")
 
@@ -202,7 +211,7 @@ def get_args_from_command_line():
         "--max_features",
         type=int,
         default=7,
-        help="Maximum number of features to use in the model",
+        help="Max number of features to use",
     )
 
     parser.add_argument(
@@ -211,12 +220,11 @@ def get_args_from_command_line():
         default=5,
         help="Number of cross-validation folds",
     )
-
     parser.add_argument(
         "--max_neighbors",
         type=int,
         default=10,
-        help="Maximum number of neighbors to use in the KNN model",
+        help="Max number of neighbors to use",
     )
 
     args = parser.parse_args()
@@ -228,17 +236,24 @@ def main():
     """Main function"""
 
     args = get_args_from_command_line()
-    x_train, x_test, y_train, y_test = load_train_and_test_datasets()
+    x_train, x_test, y_train_true, y_test_true = load_train_and_test_datasets()
 
     regressors = [
-        make_linear_regressor(max_features=args.max_features, cv=args.cv),
+        #
+        make_linear_regressor(
+            max_features=args.max_features,
+            cv=args.cv,
+        ),
+        #
         make_knn_regressor(
-            max_features=args.max_features, cv=args.cv, max_neighbors=args.max_neighbors
+            max_features=args.max_features,
+            cv=args.cv,
+            max_neighbors=args.max_neighbors,
         ),
     ]
 
     for regressor in regressors:
-        regressor.fit(x_train, y_train)
+        regressor.fit(x_train, y_train_true)
 
     best_model = load_model_from_disk()
 
@@ -247,8 +262,8 @@ def main():
         save_model_to_disk(best_model)
 
     y_test_pred = best_model.predict(x_test)
-    mse, mae, r2 = compute_metrics(y_test, y_test_pred)
-    reports_metrics(best_model, mse, mae, r2)
+    mse, mae, r2 = compute_metrics(y_test_true, y_test_pred)
+    report_metrics(best_model, mse, mae, r2)
 
 
 if __name__ == "__main__":
